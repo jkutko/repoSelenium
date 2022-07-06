2704b80274903e382ca439a268c2bac873a1c1f6
justynau <justyna.utko@ttpsc.pl>
2022-07-06 14:51:00 +0200

    Revert "tx" add to git ignore

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,34 @@
+public class TypyObiektowe {
+
+    public static void main(String[] args) {
+
+      /*
+      *  System.out.println("Hello!!!");
+      * */
+
+
+        // liczby staloprzecinkowe
+        Byte firstNumber = 123;
+        Short secondNumber = 23454;
+        Integer thirdNumber = 3343432;
+        Long fourthNumber = 2134567L;
+
+        // liczby zmiennoprzecinkowe
+        Float fifthNumber = 4.9993F;
+        Double sixnumber = 3.999999999D;
+
+        // wartość logiczna
+        Boolean prawda = true;
+        Boolean falsz = false;
+
+        // pojedynczy znak
+        Character letter = 'A';
+
+        // ciag znaków
+        String hello = "Hejka";
+
+        System.out.println(hello.charAt(0));
+        System.out.println(hello.contains("Hej"));
+
+    }
+}
---
+++ src/Zmienne.java
@@ -0,0 +1,16 @@
+// zarezerwowane miejsce z określonym typem danych (typ danych proste i obiektowe)
+
+public class Zmienne {
+
+    public static void main(String[] args) {
+        String name = "Justyna";
+        int liczba1 = 1;
+        int liczba2 = 2;
+        int wynik = liczba1 + liczba2;
+
+        name = "Inne imię";
+
+        System.out.println(name);
+        System.out.println(wynik);
+    }
+}
