593ce06e1a8f3736958f67b6342e86271ae76c41
justynau <justyna.utko@ttpsc.pl>
2023-01-20 16:26:27 +0100

    do while

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -2,14 +2,23 @@ public class whileLoop {
 
     public static void main(String[] args) {
 
-        int number;
+/*        int number;
         number = 10;
 
         while (number < 20) {
             System.out.println("Liczba " + number + " mniejsza od 20");
             number++;
         }
+        */
+// petla do while wykona się zawsze chociaż raz
+    int number;
+        number = 21;
 
+        do {
+            System.out.println("Liczba " + number + " mniejsza od 20");
+            number++;
+        }
+        while (number < 20);
 
-    }
+        }
 }
