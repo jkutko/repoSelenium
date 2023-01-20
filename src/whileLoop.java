1389c441edeb6b460e63df374c6b4617f056a8c6
justynau <justyna.utko@ttpsc.pl>
2023-01-20 16:15:31 +0100

    swith

---
+++ src/whileLoop.java
@@ -0,0 +1,15 @@
+public class whileLoop {
+
+    public static void main(String[] args) {
+
+        int number;
+        number = 10;
+
+        while (number < 20) {
+            System.out.println("Liczba " + number + " mniejsza od 20");
+            number++;
+        }
+
+
+    }
+}
